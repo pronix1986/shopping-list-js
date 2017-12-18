5608a3549a3cab2daeaa04032672094e341ff7bc
Siarhei_Prakharenka@epam.com <Wellbe1ng>
2017-12-18 12:38:02 +0300

    simple js

---
+++ app.js
@@ -0,0 +1,51 @@
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+    initForm();
+} else {
+    document.addEventListener("DOMContentLoaded", initForm);
+}
+
+
+function initForm() {
+
+    // test values:
+    localStorage.setItem('itemArr', JSON.stringify([
+        {done: false, name: "Test1", quantity: 2, price: 100},
+        {done: true, name: "Test2", quantity: 1, price: 100}
+    ]))
+
+    let itemArr = getArrFromLocalStorage('itemArr');
+    let doneItemArr = getArrFromLocalStorage('doneItemArr');
+    let deletedItemArr = getArrFromLocalStorage('deletedItemArr');
+    
+    itemArr.forEach((el, index) => addItemToTable(++index, el.done, el.name, el.quantity, el.price));
+
+    function getArrFromLocalStorage(name) {
+        return localStorage.getItem(name) ? JSON.parse(localStorage.getItem(name)) : []
+    }
+
+    function addItemToTable(index, done, name, quantity, price) {
+        let itemRow = document.createElement("tr");
+        itemRow.innerHTML = 
+        `
+            <td>${index}</td>
+            <td><input type="checkbox" name="done" value="done" ${done?"checked":""}></td>
+            <td>${name}</td>
+            <td>${quantity}</td>
+            <td>${price}</td>
+            <td>
+                <button class="button-edit" data-tooltip="Edit">
+                    <span>&#x270e;</span>
+                </button>
+                <button class="button-delete" data-tooltip="Delete">
+                    <span>&#x274c;</span>
+                </button>
+            </td>
+        `;
+        document.getElementById('items').appendChild(itemRow);
+    }
+    
+}
+
