f049f9349da4edf68b91541b04987affc56ff074
Siarhei_Prakharenka@epam.com <Wellbe1ng>
2017-12-19 10:57:05 +0300

    delete button

--- app.js
+++ app.js
@@ -10,31 +10,33 @@ if (
 function initForm() {
 
     /* test values: */
-    if(getArrFromLocalStorage('itemArr').length === 0)
-        setArrToLocalStorage('itemArr', [
+    if(getFromLocalStorage('itemArr').length === 0)
+        setToLocalStorage('itemArr', [
             {done: false, name: "Test1", quantity: 2, price: 100},
             {done: false, name: "Test2", quantity: 1, price: 100}
         ]);
-    if(getArrFromLocalStorage('doneItemArr').length === 0)
-        setArrToLocalStorage('doneItemArr', [
+    if(getFromLocalStorage('doneItemArr').length === 0)
+        setToLocalStorage('doneItemArr', [
             {done: true, name: "Test3", quantity: 20, price: 20},
             {done: true, name: "Test4", quantity: 8, price: 5}
         ]);
-    if(getArrFromLocalStorage('deletedItemArr'). length === 0)
-        setArrToLocalStorage('deletedItemArr', [
+    if(getFromLocalStorage('deletedItemArr'). length === 0)
+        setToLocalStorage('deletedItemArr', [
             {done: false, name: "Test5", quantity: 32, price: 150},
             {done: true, name: "Test6", quantity: 21, price: 10}
         ])
 
+    let sectionsArrs = {'itemArr': 'shopping-list' , 'doneItemArr': 'done-list', 'deletedItemArr': 'deleted-list'};
+    setToLocalStorage('sectionsArrs', sectionsArrs)
     initDefaultTab();
 }
 
-function getArrFromLocalStorage(name) {
-    return localStorage.getItem(name) ? JSON.parse(localStorage.getItem(name)) : []
+function getFromLocalStorage(name, isArr = true) {
+    return localStorage.getItem(name) ? JSON.parse(localStorage.getItem(name)) : (isArr? [] : {})
 }
 
-function setArrToLocalStorage(name, arr) {
-    localStorage.setItem(name, JSON.stringify(arr));
+function setToLocalStorage(name, obj) {
+    localStorage.setItem(name, JSON.stringify(obj));
 }
 
 function clearTable() {
@@ -51,7 +53,7 @@ function addItemToTable(index, done, name, quantity, price) {
         <td>${price}</td>
         <td>
             <div class="button-group">
-                <button class="button-edit" data-tooltip="Edit">
+                <button class="button-edit" data-tooltip="Edit" onclick="editItem(event)">
                     <span>&#x270e;</span>
                 </button>
                 <button class="button-delete" data-tooltip="Delete" onclick="deleteItem(event)">
@@ -72,18 +74,12 @@ function displayTabHeader(tabName) {
     scrollTo(0,0);
 }
 
-function displayDeleteButtons(tabName) {
-    let delBtns = [...document.getElementsByClassName("button-delete")];
-    delBtns.forEach(b => tabName!=='deleted-list' ? b.style.display = 'initial' : b.style.display = 'none');
-}
-
 function displayEditButtons(tabName) {
     let editBtns = [...document.getElementsByClassName("button-edit")];
     editBtns.forEach(b => tabName==='shopping-list' ? b.style.display = 'initial' : b.style.display = 'none');
 }
 
 function updateButtons(tabName) {
-    displayDeleteButtons(tabName);
     displayEditButtons(tabName);
 }
 
@@ -95,16 +91,15 @@ function updateMenubar(newActiveElement, oldActiveElement) {
 function updateResultTable(tabName) {
     updateCurrentArrName(tabName);
     clearTable();
-    let curArr = getArrFromLocalStorage(getCurrentArrName());
+    let curArr = getFromLocalStorage(getCurrentArrName());
     curArr.forEach((el, index) => addItemToTable(++index, el.done, el.name, el.quantity, el.price));
     updateButtons(tabName);
-    let total = curArr.reduce((acc, curVal) => acc+= curVal.price, 0);
+    let total = curArr.reduce((acc, curVal) => acc+= (+curVal.price), 0);
     addTotal(total);
 }
 
 function initDefaultTab() {
-    displayTabHeader('shopping-list');
-    updateResultTable('shopping-list');
+    refreshPage('shopping-list')
 }
 
 function handleTabClick(event) {
@@ -116,6 +111,11 @@ function handleTabClick(event) {
         updateMenubar(target, old);
         updateResultTable(sectionName);
     }
+}
+
+function refreshPage(sectionName) {
+    displayTabHeader(sectionName);
+    updateResultTable(sectionName);
 }
 
 function updateCurrentArrName(tabName) {
@@ -142,14 +142,22 @@ function getCurrentArrName() {
 
 function deleteItem(event) {
     let id = getIdFromEvent(event);
-    let deletedItemArr = getArrFromLocalStorage('deletedItemArr');
-    let curArr = getArrFromLocalStorage(getCurrentArrName());
+    let deletedItemArr = getFromLocalStorage('deletedItemArr');
+    let curArrName = getCurrentArrName();
+    let curArr = getFromLocalStorage(curArrName);
     let itemToRemove = curArr[id];
-    deletedItemArr.push(itemToRemove);
-    setArrToLocalStorage('deletedItemArr', deletedItemArr);
-    curArr.splice(id, 1);
-    setArrToLocalStorage(getCurrentArrName(), curArr);
-    location.reload();
+    if('deletedItemArr' !== curArrName) { 
+        deletedItemArr.push(itemToRemove);
+        setToLocalStorage('deletedItemArr', deletedItemArr);
+        curArr.splice(id, 1);
+        setToLocalStorage(curArrName, curArr);
+    } else {
+        if(window.confirm('Delete the item permanently?')) {
+            deletedItemArr.splice(id, 1);
+            setToLocalStorage('deletedItemArr', deletedItemArr);
+        }
+    }
+    refreshPage(getFromLocalStorage('sectionsArrs', false)[curArrName]);
 }
 
 function findParentNodeByTag(parentTagName, childObj) {
@@ -160,8 +168,44 @@ function findParentNodeByTag(parentTagName, childObj) {
     return testObj;
 }
 
-function getIdFromEvent(event) {
+function getIdFromEvent(event, setActive = false) {
     let tr = findParentNodeByTag("tr", event.target);
+    if(setActive) {
+        tr.classList.add("active");
+    }
     return Number(tr.firstElementChild.innerHTML) - 1;
+}
+
+function editItem(event) {
+    let id = getIdFromEvent(event, true);
+    let 
+}
+
+function addItem(some) {
+    let form = findParentNodeByTag("form", event.target);
+    let fName = form.elements[0].value;
+    let fQuantity = form.elements[1].value;
+    let fPrice = form.elements[2].value;
+    let item = {done: false, name: fName, quantity: fQuantity, price: fPrice}
+    let validationMessage = validateItem(item)
+    if(validationMessage) {
+      //  console.log(`TODO: item validation message: ${validationMessage}`);
+        alert(validationMessage);
+        return false;
+    }
+    let itemArr = getFromLocalStorage('itemArr');
+    itemArr.push(item);
+    setToLocalStorage('itemArr', itemArr);
+    location.reload();
+}
+
+function validateItem(item) {   // TODO
+    if(!item.name || !item.quantity || !item.price) {
+        return "All fields are required";
+    }
+    if(isNaN(item.price) || item.price <= 0) {
+        return "'Price' is invalid";
+    }
+    return "";                
 }
 
